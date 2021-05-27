abd5f01005915418c2d3df049124af7536eca76c
MOHIT CHAUHAN <mohit.99chauhan@gmail.com>
2021-05-27 13:49:25 +0530

    taylor series and hello world program

---
+++ taylor_series.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+double taylor(int x , int n){
+    static double p=1,q=1;
+    double r;
+    if(n==0)
+        return 1;
+    else{
+        r=taylor(x , n-1);
+        p=p*x;
+        q=q*n;
+        return r+p/q;
+    }
+}
+
+int main(){
+   printf("%lf",taylor(4,10));
+}
